1db89036ad50e52155900d293bf44d4669520431
labmente <40374766+labmente@users.noreply.github.com>
2021-06-21 00:21:07 -0300

    Add files via upload
    Inicialização do projeto, configuração de leitura do Typescript

---
+++ index.js
@@ -0,0 +1,4 @@
+function enviarEmail(nome, email) {
+    console.log("Enviar email para " + "Wesley" + " com o email " + "labmente@gmail.com");
+}
+enviarEmail("WESLEY", "labmente@gmail.com");
---
+++ index.ts
@@ -0,0 +1,4 @@
+function enviarEmail(nome: string, email: string){
+  console.log(`Enviar email para ${"Wesley"} com o email ${"labmente@gmail.com"}`);
+}
+enviarEmail("WESLEY", "labmente@gmail.com");
